14460ca6ba564cce0f9afe6e56d54595b6e81df1
Jake Brackney <brackney.jake@gmail.com>
2019-03-20 15:56:18 -0400

    Deployment Almost Successful

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/JakeBrackney/the-giving-list/precache-manifest.8eeaa7425e35a0c836b47533e95fb0f5.js"
+  "/precache-manifest.f4d66d906540854b35fc8f9f13b97cac.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/JakeBrackney/the-giving-list/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
