be5c47cd00ae3affaf311cae616ef9b4c848174e
Jake Brackney <brackney.jake@gmail.com>
2019-03-20 14:21:32 -0400

    Deployment experiment 2

---
+++ build/precache-manifest.8eeaa7425e35a0c836b47533e95fb0f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33a91948c30913209818",
+    "url": "/JakeBrackney/the-giving-list/static/css/main.367ac0a6.chunk.css"
+  },
+  {
+    "revision": "33a91948c30913209818",
+    "url": "/JakeBrackney/the-giving-list/static/js/main.710b5baa.chunk.js"
+  },
+  {
+    "revision": "1ebe82f2e562c0e955a5",
+    "url": "/JakeBrackney/the-giving-list/static/js/runtime~main.07d3ff18.js"
+  },
+  {
+    "revision": "c14d85c9014139fb6670",
+    "url": "/JakeBrackney/the-giving-list/static/js/2.be6cffc3.chunk.js"
+  },
+  {
+    "revision": "08791636ceeaedb17483ccfeb262e68f",
+    "url": "/JakeBrackney/the-giving-list/index.html"
+  }
+];
