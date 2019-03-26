284ee482341e97ba353e2c73619ca1ddd2553a90
Jake Brackney <brackney.jake@gmail.com>
2019-03-26 14:44:32 -0400

    Got list of charities to render alphabetically WHOOOOO

--- src/Components/CharityList/CharityList.js
+++ src/Components/CharityList/CharityList.js
@@ -4,23 +4,23 @@ import './CharityList.css'
 class CharityList extends Component {
 
   render() {
-    if(this.props.charities) {
-    let list = this.props.charities.map(index => {
-        return (
-        <div key={index.org}>
-            <ul>
-                <li><a href={"giving/" + index._id}>{index.org}</a></li>
-            </ul>
-        </div>
-        )      
-    })
+
+        let list = [...this.props.charities]
+        .sort((a, b) => (a.org > b.org)-1)
+        .map((index, i) => 
+            <div key={i}>
+                <ul>
+                    <li><a href={"giving/" + index._id}>{index.org}</a></li>
+                </ul>
+            </div>
+    )
     return (
         <div className='charityList'>
             <h1>Organizations</h1>
             {list}
         </div>
-    );}
+    )}
 }
-}
+
 
 export default CharityList;
